ffee1c4a2cde448f5331b9a077fbe9a3b244878c
annusgit <annusgit@gmail.com>
2018-02-24 23:37:26 +0500

    running but can't follow the tail

--- Game.py
+++ Game.py
@@ -2,47 +2,52 @@
 
 from __future__ import print_function
 from __future__ import division
+
+from six.moves import xrange
 import pygame
 from random import randint
 
 class Snake(pygame.sprite.Sprite):
     # directions = ['l', 'r', 'u', 'd']
-    def __init__(self, color, size, length=3, width=600, height=600):
+    def __init__(self, color, seg_color, size, speed, length=3, width=600, height=600):
         super(Snake,self).__init__()
-        self.image = pygame.Surface([size[0],size[1]])
+        self.image = pygame.Surface(size)
         self.image.fill(color=color)
         self.rect = self.image.get_rect()
-        self.rect.x = 0
-        self.rect.y = 0
-        self.dir = 'l'
+        self.rect.x = randint(20, width - 20)
+        self.rect.y = randint(20, height - 20)
+        self.speed = speed
+        self.dt = 1
+        self.dir = 'r'
         self.w = width
         self.h = height
+        self.seg_color = seg_color
+        self.size = size
+        self.score = 0
         return
 
-    def this_key(self, key):
-        self.key = key
-        # print(self.key)
-
     def update(self, *args):
-        # print(self.key)
-        if self.key:
-            if self.key == 273:
+        [key, eaten] = args
+        if key:
+            if key == 273:
                 self.dir = 'u'
-            elif self.key == 275:
+            elif key == 275:
                 self.dir = 'r'
-            elif self.key == 274:
+            elif key == 274:
                 self.dir = 'd'
-            elif self.key == 276:
+            elif key == 276:
                 self.dir = 'l'
+        if eaten:
+            self.score += 1
 
         if self.dir == 'r':
-            self.rect.x += 1
+            self.rect.x += self.speed*self.dt
         elif self.dir == 'l':
-            self.rect.x -= 1
+            self.rect.x -= self.speed*self.dt
         elif self.dir == 'u':
-            self.rect.y -= 1
+            self.rect.y -= self.speed*self.dt
         elif self.dir == 'd':
-            self.rect.y += 1
+            self.rect.y += self.speed*self.dt
 
         if self.rect.x > self.w:
             self.rect.x = 0
@@ -53,14 +58,47 @@ class Snake(pygame.sprite.Sprite):
         elif self.rect.y < 0:
             self.rect.y = self.h-1
 
-        print(self.rect.x, self.rect.y)
-        pass
+    def get_score(self):
+        return self.score
+
+
+class Segment(pygame.sprite.Sprite):
+    def __init__(self, seg_color, size, pos, number, direction, distance):
+        super(Segment, self).__init__()
+        self.image = pygame.Surface(size)
+        self.image.fill(color=seg_color)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = pos
+        self.d = distance
+        if direction == 'l':
+            self.rect.x += self.d
+        elif direction == 'r':
+            self.rect.x -= self.d
+        elif direction == 'u':
+            self.rect.y += self.d
+        else:
+            self.rect.y -= self.d
+
+        self.number = number
+        return
+
+    def update(self, *args):
+        snake, sprites = args
+        if self.number == 0:
+            self.rect.x = snake.rect.x
+            self.rect.y = snake.rect.y
+            # print(self.rect, snake.rect)
+        else:
+            self.rect.x = sprites[self.number-1].rect.x
+            self.rect.y = sprites[self.number-1].rect.y
+            # print(self.rect, sprites[self.number-1].rect)
+        return
 
 
 class Food(pygame.sprite.Sprite):
     def __init__(self, color, size, width=600, height=600):
         super(Food, self).__init__()
-        self.image = pygame.Surface([size[0], size[1]])
+        self.image = pygame.Surface(size)
         self.image.fill(color)
         self.rect = self.image.get_rect()
         self.w = width
@@ -70,17 +108,16 @@ class Food(pygame.sprite.Sprite):
         self.score = 0
         return
 
-    def this_status(self, status):
-        self.status = status
-
     def update(self, *args):
-        if self.status == 1:
-            self.score += 1
-            print(self.score)
-            self.rect.x = randint(0, self.w)
-            self.rect.y = randint(0, self.h)
+        status = args[1]
+        if status == 1:
+            self.rect.x = randint(20, self.w-20)
+            self.rect.y = randint(20, self.h-20)
         return
 
+
+def get_index(seg):
+    return seg.number
 
 
 
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 
 from __future__ import print_function
 from __future__ import division
-from Game import Snake, Food
+from Game import Snake, Segment, Food, get_index
 import pygame
 import time
-
-
 colors = {'white': (255,255,255), 'black': (0,0,0), 'red':(255,0,0),
           'green': (0,255,0), 'blue': (0,0,255)}
 
@@ -13,32 +11,48 @@ def main():
     scr_width=600
     scr_height=600
     pygame.init()
+    pygame.font.init()
+    myfont = pygame.font.SysFont(None, 30)
     screen = pygame.display.set_mode([scr_width,scr_height])
+    pygame.display.set_caption('snakes')
     done = False
-    snake = Snake(color=colors['blue'], size=(10, 10), width=scr_width, height=scr_height)
+    snake = Snake(color=colors['blue'], seg_color=colors['green'], size=(20, 20),
+                  speed=2, width=scr_width, height=scr_height)
     food = Food(color=colors['red'], size=(10, 10), width=scr_width, height=scr_height)
-    sprites = pygame.sprite.Group()
-    sprites.add(snake)
-    # sprites.add(food)
-    delay=0.003
+    all_sprites = pygame.sprite.Group()
+    all_sprites.add(snake)
+    all_sprites.add(food)
+    snake_body = pygame.sprite.Group()
+    delay = 0.003
     while not done:
-        key = None
+        key_pressed = None
         for event in pygame.event.get():
-            # print(event)
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
                 done = True
             elif event.type == pygame.KEYDOWN:
-                key = pygame.key.get_pressed()
-                key = key.index(1)
+                key_pressed = event.key
         screen.fill(colors['white'])
-        hit = pygame.sprite.spritecollide(snake, sprites, True)
-        # print(hit)
-        snake.this_key(key)
-        # food.this_status(hit)
-        sprites.draw(screen)
-        sprites.update()
+        eaten = pygame.sprite.collide_rect(snake, food)
+        snake_body.draw(screen)
+        all_sprites.draw(screen)
+        sorted_segments = sorted(snake_body.sprites(),key=get_index)
+        if eaten:
+            if len(snake_body) == 0:
+                snake_body.add(Segment(seg_color=colors['green'], size=(20, 20), pos=(snake.rect.x, snake.rect.y),
+                                    number=len(snake_body), direction=snake.dir, distance=30))
+            else:
+                snake_body.add(Segment(seg_color=colors['green'], size=(20, 20),
+                                       pos=(sorted_segments[-1].rect.x, sorted_segments[-1].rect.y),
+                                       number=len(snake_body), direction=snake.dir, distance=30))
+        snake_body.update(snake, sorted_segments)
+        all_sprites.update(key_pressed, eaten)
+        score = snake.get_score()
+        textsurface = myfont.render('score: {}, segments: {}'.format(score, len(all_sprites)-2), True, colors['black'])
+        screen.blit(textsurface, (0, scr_height-20))
         pygame.display.flip()
         time.sleep(delay)
+    # for s in sorted_segments:
+    #     print(s.rect)
     return
 
 
